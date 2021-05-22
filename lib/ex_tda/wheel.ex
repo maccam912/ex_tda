9041715ea943583425d8a499c8d8562e904f23e9
maccam912 <maccam912@gmail.com>
2021-05-22 07:36:31 -0500

    using sqlite for table

--- lib/ex_tda/wheel.ex
+++ lib/ex_tda/wheel.ex
@@ -1,30 +1,40 @@
 defmodule ExTda.Wheel do
   def suggest_contract(symbol) do
+    # {:ok, conn} = Exqlite.Sqlite3.open(":memory:")
+    {:ok, conn} = Exqlite.Sqlite3.open("puts.db")
+
+    :ok =
+      Exqlite.Sqlite3.execute(
+        conn,
+        "create table puts (symbol text, dte integer, bid decimal, delta decimal, strike decimal, exp text, prem_per_day decimal GENERATED ALWAYS AS (bid/dte) VIRTUAL)"
+      )
+
     %{puts: puts} = ExTda.Client.get_option_chain(symbol, "PUT", "OTM")
 
-    best =
-      puts
-      |> Stream.map(fn {dt, contracts} ->
-        [exp, dte] = String.split(dt, [":"])
+    puts
+    |> Stream.map(fn {dt, contracts} ->
+      [exp, dte] = String.split(dt, [":"])
 
-        {best_prem, strike} =
-          IO.inspect(
-            contracts
-            |> Stream.filter(fn {_strike, [%{"delta" => delta}]} ->
-              delta > -0.4 && delta < -0.2
-            end)
-            |> Stream.map(fn {strike, %{"bidPrice" => bid}} ->
-              {bid / dte, strike}
-            end)
-            |> Enum.sort_by(fn {prem, _} -> prem end)
-            |> List.first()
-          )
+      values_lines =
+        contracts
+        |> Stream.map(fn {strike, [%{"bid" => bid, "delta" => delta}]} ->
+          if delta != "NaN" do
+            # "(#{dte}, #{bid}, #{delta}, #{strike}, '#{exp}')"
+            query =
+              IO.inspect(
+                "insert into puts (symbol, dte, bid, delta, strike, exp) values ('#{symbol}', #{dte}, #{bid}, #{delta}, #{strike}, '#{exp}')"
+              )
 
-        {best_prem, exp, strike}
-      end)
-      |> Enum.sort_by(fn {best_prem, _, _} -> best_prem end)
-      |> List.first()
+            :ok = Exqlite.Sqlite3.execute(conn, query)
+          end
+        end)
+        |> Enum.join(",")
 
-    best
+      # query = IO.inspect "insert into puts (dte, bid, delta, strike, exp) values #{values_lines}"
+      # :ok = Exqlite.Sqlite3.execute(conn, query)
+    end)
+    |> Stream.run()
+
+    conn
   end
 end
